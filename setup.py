fe376264fddd06d332b16d1a2191065e9f9e6178
dantezhu <dantezhu@qq.com>
2019-04-11 11:15:28 +0800

    unidecode

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
     url=zinnia_wordpress.__url__,
 
     packages=find_packages(exclude=['demo_zinnia_wordpress']),
+    install_requires=['unidecode'],
     classifiers=[
         'Framework :: Django',
         'Development Status :: 5 - Production/Stable',
